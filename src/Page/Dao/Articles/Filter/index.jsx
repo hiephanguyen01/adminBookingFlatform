0c3a6e33e3d5be3e31d5e16a5c8e40be3936483e
KhoaLang <tkhoa882@gmail.com>
2023-05-26 20:18:52 +0700

    sua loi filter theo ngay

--- src/Page/Dao/Articles/Filter/index.jsx
+++ src/Page/Dao/Articles/Filter/index.jsx
@@ -11,12 +11,8 @@ const Filter = (props) => {
         setConditionSelected([...conditionSelected, dateStrings]);
       }
     } else {
-      setConditionSelected([
-        ...conditionSelected.filter((item) => {
-          if (item[0] !== undefined && item[0].includes("-")) return false;
-          return true;
-        }),
-      ]);
+      console.log("Xóa");
+      setConditionSelected([...conditionSelected.slice(0, -1)]);
     }
   };
   return (
